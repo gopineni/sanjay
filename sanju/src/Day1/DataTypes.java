191b9fbb866640ae71c6d89747e10409734eefc5
GopineniSanjay <2001gopinenisanjay2001@gmail.com>
2023-12-26 12:22:16 +0530

    2nd commit

--- sanju/src/Day1/DataTypes.java
+++ sanju/src/Day1/DataTypes.java
@@ -5,7 +5,7 @@ public class DataTypes {
 	public static void main(String[] args) {
 		int value1 = 9 / 2; //integer division
 		float value2 = 101 / 61f;
-		double value3 = 10d / 6d0;
+		double value3 = 10d / 6d;
 		float value4 = 5/2.5f; //float division
 		System.out.println("value 1=" + value1); 
 		System.out.println("value 2=" + value2);
